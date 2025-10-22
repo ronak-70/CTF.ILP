afce7e2f07c09ee250cd1f869d10cda990e9c635
Rounak Saeedi <ronaksaeedi65@gmail.com>
2025-10-22 13:25:12 +0200

    new

---
+++ projekt_021.js
@@ -0,0 +1,15 @@
+//for(let i = 10, i >= 1, i-- ){
+//console.log(i);
+////}
+//for (let i = 10; i >= 1; i--) {
+// console.log(i);
+//}
+//console.log("Start!");
+function start() {
+  let name = "rounak ";
+  let age = 33;
+  const ergbis = name + age + " willkommen";
+  return ergbis;
+}
+const ergbis = start();
+console.log(ergbis);
